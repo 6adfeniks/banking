ea3793f4cc63423042cbf0b9042ba22bae7251f5
Kolya Akulich <6adfeniks@gmail.com>
2022-02-21 20:26:20 +0300

    m

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -4,14 +4,9 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"github.com/6adfeniks/banking/service"
+	"github.com/gorilla/mux"
 	"net/http"
 )
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zipcode"`
-}
 
 type CustomerHandlers struct {
 	service service.CustomerService
@@ -31,6 +26,27 @@ func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Reque
 	} else {
 		w.Header().Add("Content-type", "application/json")
 		json.NewEncoder(w).Encode(customers)
+	}
+
+}
+
+func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["customer_id"]
+
+	customer, err := ch.service.GetCustomer(id)
+	if err != nil {
+		writeResponse(w, err.Code, err.AsMessage())
+	} else {
+		writeResponse(w, http.StatusOK, customer)
+	}
+}
+
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
 	}
 
 }
---
+++ domain/CustomerRepositoryDB.go
@@ -0,0 +1,68 @@
+package domain
+
+import (
+	"database/sql"
+	"github.com/6adfeniks/banking/errs"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"time"
+)
+
+type CustomerRepositoryDb struct {
+	client *sql.DB
+}
+
+func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
+
+	findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
+
+	rows, err := d.client.Query(findAllSql)
+	if err != nil {
+		log.Panicln("Error while querying customer table" + err.Error())
+		return nil, err
+	}
+	customers := make([]Customer, 0)
+	for rows.Next() {
+		var c Customer
+		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBirth, &c.Status)
+		if err != nil {
+			log.Println("Error while scanning customers " + err.Error())
+			return nil, err
+		}
+
+		customers = append(customers, c)
+	}
+	return customers, nil
+}
+
+func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
+	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
+
+	row := d.client.QueryRow(customerSql, id)
+	var c Customer
+	err := row.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBirth, &c.Status)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errs.NewFoundError("Customer not found")
+		} else {
+			log.Println("Error while scanning customer by id " + err.Error())
+			return nil, errs.NewUnexpectedError("Unexpected database error")
+		}
+
+	}
+	return &c, nil
+
+}
+
+func NewCustomerRepositoryDb() CustomerRepositoryDb {
+	client, err := sql.Open("mysql", "root:password@/banking") // enter the password
+	//client, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return CustomerRepositoryDb{client}
+}
